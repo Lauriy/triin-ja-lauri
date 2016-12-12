43df0424b7d2e3a13ecd393f7162cbc94f29c71e
Lauri Elias <laurileet@gmail.com>
2016-12-12 21:23:51 +0200

    Update live run command

--- production-uwsgi.sh
+++ production-uwsgi.sh
@@ -3,5 +3,5 @@ export HOME=/home/triinjalauri
 cd $HOME
 source venv/bin/activate
 cd $HOME/triinjalauri/
-uwsgi --socket /home/triinjalauri/triinjalauri.sock --module triinjalauri.wsgi --chmod-socket=777 --env=LANG="en_US.utf8"
+uwsgi --socket /home/triinjalauri/triinjalauri.sock --processes=1 --module triinjalauri.wsgi --chmod-socket=777 --env=LANG="en_US.utf8"
 exit $?
